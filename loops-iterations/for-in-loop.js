e3d44e124ff16091e8a52e0151de13cdeea444b3
Georgina <georginamircheska@gmail.com>
2025-08-06 11:34:31 +0200

    Exer: for...in loop 02 exercise

--- loops-iterations/for-in-loop.js
+++ loops-iterations/for-in-loop.js
@@ -1,6 +1,6 @@
 //EXERCISES
 
-// You’re given an object that represents a book. Write a function called describeBook() that takes a book object and logs each property and its value in this format: title: The Hobbit; author: J.R.R. Tolkien; year: 1937;
+//01 - You’re given an object that represents a book. Write a function called describeBook() that takes a book object and logs each property and its value in this format: title: The Hobbit; author: J.R.R. Tolkien; year: 1937;
 
 const book = {
   title: "The Hobbit",
@@ -10,8 +10,31 @@ const book = {
 
 function describeBook() {
   for (let prop in book) {
-    console.log(`${prop}: ${book[prop]}`);
+    //console.log(`${prop}: ${book[prop]}`);
   }
 }
 
 describeBook();
+
+//02 - The function giveMeFive accepts 1 parameter, obj, which is an object. Create an array (which you will eventually return). Then, traverse obj, checking each key and value. If the length of the key is equal to 5, then push the key to your array. Separately, if the length of the value is equal to 5, then push the value to your array. At the end, return your array. You should use for..in in your code, otherwise your solution may not pass this kata.
+
+const obj = {
+  spring: "april",
+  summer: "august",
+  fruit: "apple",
+  vegetable: "tomato",
+};
+
+function giveMeFive(obj) {
+  const arr = [];
+  for (let key in obj) {
+    if (key.length === 5) {
+      arr.push(key);
+    }
+    if (obj[key].length === 5) {
+      arr.push(obj[key]);
+    }
+  }
+  return arr;
+}
+console.log(giveMeFive(obj));
--- loops-iterations/for-of-loop.js
+++ loops-iterations/for-of-loop.js
@@ -1,5 +1,5 @@
 //EXERCISES
-// Create a function called sumArray that takes an array of numbers and returns the total sum of all the numbers using a for...of loop.
+//01 - Create a function called sumArray that takes an array of numbers and returns the total sum of all the numbers using a for...of loop.
 
 let numbers = [1, 2, 4, 6, 8];
 
@@ -13,7 +13,7 @@ function sumArray(numbers) {
 }
 console.log(sumArray(numbers));
 
-//Challenge: Print Names of Active Users.You have an array of user objects. Each user has a name and an isActive boolean. Write a function printActiveUsers(users) that: Loops through the array using a for...of loop.Prints only the names of users whose isActive property is true.
+//02 - Challenge: Print Names of Active Users.You have an array of user objects. Each user has a name and an isActive boolean. Write a function printActiveUsers(users) that: Loops through the array using a for...of loop.Prints only the names of users whose isActive property is true.
 
 const users = [
   { name: "Alice", isActive: true },
@@ -30,3 +30,30 @@ function printActiveUsers(users) {
 }
 
 printActiveUsers(users);
+
+//03 - Challenge: Average Score of Passed Students. You have an array of student objects. Each student has a name, a scores array, and a passed boolean. Write a function that: Loops through the students. Filters only the students who passed (passed: true). Calculates the average score for each passed student. Returns an array of strings with the format: "StudentName: averageScore".
+
+const students = [
+  { name: "Ana", scores: [80, 90, 85], passed: true },
+  { name: "Ben", scores: [60, 65, 70], passed: false },
+  { name: "Cara", scores: [88, 92, 84], passed: true },
+];
+
+function passedStudents(students) {
+  let result = [];
+
+  for (let student of students) {
+    if (student.passed === true) {
+      let sum = 0;
+      for (let score of student.scores) {
+        sum += score;
+      }
+
+      let avrScore = sum / student.scores.length;
+      result.push(`${student.name}: ${avrScore}`);
+    }
+  }
+  return result;
+}
+
+console.log(passedStudents(students));
